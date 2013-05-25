57cbd273c6e920b90e6fc1003e29c6c2d61c1a1f
LitDoggy <1297715457@qq.com>
2013-05-25 10:19:01 +0800

    new
    123

---
+++ 2.13.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{   
+	int a;
+	scanf("%d",&a);
+	if(a<=2) 
+    printf(" 3 ");
+    else
+    printf("4");
+    system (" pause");
+    return 0;
+}
---
+++ 4.1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int a,b,sun;
+    scanf("%d %d",&a,&b);
+    sun=a*b;
+    
+	printf("%d",sun);
+
+	system (" pause");
+	return 0;}
